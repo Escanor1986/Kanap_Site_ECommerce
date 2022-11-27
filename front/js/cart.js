8b90cacfbfd045272b5b9360153cef736a93c145
Escanor1986 <lionel.zovi@gmail.com>
2022-11-27 08:53:22 +0100

    Beginning of cart.js

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,1 +1,18 @@
 console.log(localStorage);
+
+let productOrderCart = [];
+
+productOrderCart = JSON.parse(localStorage.getItem("products"));
+console.log(productOrderCart);
+
+productOrderCart.map((product) => {
+    console.log(product.name);
+
+    const article = document.createElement("article");
+    document.getElementsById("cart__items").appendChild(article);
+    article.classList.add("cart_item");
+    article.setAttribute("data-id", product._id);
+    article.setAttribute("data-color", product.colors);
+    
+
+});
